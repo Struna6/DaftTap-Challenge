f21fdbb937e42a79baac184ec9f9aff6eae6ae8c
Struna6 <struniawski.karol@gmail.com>
2019-05-06 20:15:05 +0200

    Initial Commit

---
+++ DaftTap Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DaftTap Challenge
+//
+//  Created by Karol Struniawski on 06/05/2019.
+//  Copyright © 2019 Karol Struniawski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
